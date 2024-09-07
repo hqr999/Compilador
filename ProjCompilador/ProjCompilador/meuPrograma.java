059ee1a621fb832d10b9310032d991299e1951c4
Henrique <101482422+hqr999@users.noreply.github.com>
2024-09-07 16:56:07 -0300

    Add files via upload

---
+++ ProjCompilador/ProjCompilador/meuPrograma.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class meuPrograma{ 
+	public static void main(String args[]){ 
+	Scanner _scTrx = new Scanner(System.in);
+	int a;
+	int b;
+	String x;
+	String y;
+System.out.println("Hello World");
+System.out.println("Fim do programa");
+a=_scTrx.nextInt();
+b=_scTrx.nextInt();
+System.out.println(a);
+while (b<5) {
+b=b+5;
+while (a<5) {
+a=3+2+5;
+}
+if (b>5) {System.out.println("b maior que 5");
+}else{System.out.println("b menor que 5");
+}}
+if (a>5) {if (b<5) {System.out.println("a maior que 5 e b menor que 5");
+while (b<5) {
+b=b*2;
+}
+}else{System.out.println("a maior que 5");
+}}else{System.out.println("menor que 5");
+}while (a>5) {
+a=a-5;
+}
+System.out.println("oi agora vamos testar outro");
+if (b>=0) {System.out.println("b positivo");
+}else{System.out.println("b negativo");
+} }
+ }
---
+++ ProjCompilador/ProjCompilador/src/io/compiler/core/exception/UFABCSemanticException.java
@@ -0,0 +1,7 @@
+package io.compiler.core.exception;
+
+public class UFABCSemanticException extends RuntimeException {
+	public UFABCSemanticException (String message) {
+		super(message);
+	}
+}
